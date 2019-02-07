0b50462985f2e128774d2b2f0f5dc2d56ab8dd23
byronap <byronap@coinigy.com>
2019-02-06 19:26:03 -0500

    v 2  adds async and ability to set the textwriter

--- OutputConsole/OutputConsole.cs
+++ OutputConsole/OutputConsole.cs
@@ -1,65 +1,74 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Threading.Tasks;
 
-namespace Coinigy.Common
+public static class OutputConsole
 {
-    public static class OutputConsole
+    public static TextWriter Writer { get; set; }
+    public static bool IsWriterConsole { get; set; }
+
+    private static readonly BlockingCollection<Tuple<string, ConsoleColor?, ConsoleColor?>> MQueue = new BlockingCollection<Tuple<string, ConsoleColor?, ConsoleColor?>>();
+
+    static OutputConsole()
     {
-        private static readonly BlockingCollection<Tuple<string, ConsoleColor?, ConsoleColor?>> MQueue = new BlockingCollection<Tuple<string, ConsoleColor?, ConsoleColor?>>();
+        Writer = Console.Out;
+        IsWriterConsole = true;
 
-        static OutputConsole()
+        try
         {
-            try
-            {
-                var thread = new Thread(
-                        () =>
+            var thread = new Thread(
+                    () =>
+                    {
+                        var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+                        while (true)
                         {
-                            while (true)
+                            var (message, foreColor, backColor) = MQueue.Take();
+                            try
                             {
-                                var item = MQueue.Take();
-                                try
+                                if (isWindows && IsWriterConsole)
                                 {
-                                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                                    {
-                                        if (item.Item2 != null)
-                                            Console.ForegroundColor = (ConsoleColor) item.Item2;
-                                        if (item.Item3 != null)
-                                            Console.BackgroundColor = (ConsoleColor) item.Item3;
-                                    }
-                                }
-                                catch
-                                {
-                                    // ignore
-                                }
-
-                                Console.WriteLine(item.Item1);
-                                try
-                                {
-                                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                                        Console.ResetColor();
-                                }
-                                catch
-                                {
-                                    // ignore
+                                    if (foreColor != null)
+                                        Console.ForegroundColor = (ConsoleColor)foreColor;
+                                    if (backColor != null)
+                                        Console.BackgroundColor = (ConsoleColor)backColor;
                                 }
                             }
+                            catch
+                            {
+                                // ignore
+                            }
 
-                            // ReSharper disable once FunctionNeverReturns
-                        })
-                    {IsBackground = true};
-                thread.Start();
-            }
-            catch (Exception)
-            {
-                // ignored
-            }
+                            Writer.WriteLine(message);
+                            try
+                            {
+                                if (isWindows && IsWriterConsole)
+                                    Console.ResetColor();
+                            }
+                            catch
+                            {
+                                // ignore
+                            }
+                        }
+                    })
+            { IsBackground = true };
+            thread.Start();
         }
-
-        public static void WriteLine(string value, ConsoleColor? foregroundColor = null, ConsoleColor? backgroundColor = null)
+        catch (Exception)
         {
-            MQueue.Add(new Tuple<string, ConsoleColor?, ConsoleColor?>(value, foregroundColor, backgroundColor));
+            // ignored
         }
+    }
+
+    public static void WriteLine(string value, ConsoleColor? foregroundColor = null, ConsoleColor? backgroundColor = null)
+    {
+        MQueue.Add(new Tuple<string, ConsoleColor?, ConsoleColor?>(value, foregroundColor, backgroundColor));
+    }
+
+    public static Task WriteLineAsync(string value, ConsoleColor? foregroundColor = null, ConsoleColor? backgroundColor = null)
+    {
+        return Task.Run(() => { MQueue.Add(new Tuple<string, ConsoleColor?, ConsoleColor?>(value, foregroundColor, backgroundColor)); });
     }
 }
